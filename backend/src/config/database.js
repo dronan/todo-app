20bc3514326cfe53ea3677cb7111d1d1f92c383b
Diego Ronan Kulian <dkulian@veepee.com>
2023-06-23 17:12:15 +0200

    fix warning

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,5 +2,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+// Set the strictQuery option to false
+mongoose.set('strictQuery', true);
+
 // Connect to the database
 module.exports = mongoose.connect('mongodb://localhost/todo');
