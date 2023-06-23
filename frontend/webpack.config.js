92627c2da8e3f8715e2d2c7d087797d5e6e4858b
Diego Ronan Kulian <dkulian@veepee.com>
2023-06-23 17:33:10 +0200

    package initialize + webpack setup

---
+++ frontend/webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
+};
