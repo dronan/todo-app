d9e8d43a259fa6791427e6d8803413444d41cb44
Diego Ronan Kulian <dkulian@veepee.com>
2023-06-23 17:46:24 +0200

    create initial structure

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,27 +4,37 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
-        publicPath: '/dist'
+        path: __dirname + '/public',
+        filename: './app.js',
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
-                })
-            }
-        ]
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        },
     },
     plugins: [
         new ExtractTextPlugin('style.css')
-    ]
+    ],
+    module: {
+        loaders: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread'],
+            }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            loader: 'file'
+        }]
+    }
 };
