be8303d9fdd580050877b281d16ffb7a88c71616
Diego Ronan Kulian <dkulian@veepee.com>
2023-06-27 14:56:50 +0200

    udpate about us

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -3,12 +3,36 @@ import PageHeader from "../template/pageHeader";
 
 export default props => (
     <div>
-        <PageHeader name="About" small="About us"></PageHeader>
-        <h2>Our history</h2>
-        <p>Lorem ipsum dolor sit amet...</p>
-        <h2>Mission and Vision</h2>
-        <p>Lorem ipsum dolor sit amet...</p>
-        <h2>Press</h2>
-        <p>Lorem ipsum dolor sit amet...</p>
+        <PageHeader name="About" small="About this system"></PageHeader>
+        <h2>Porpouse</h2>
+        <p>It's a simple task manager.</p>
+        <p>It was developed with React with Redux making communications between components on a easy way.</p>
+        <h2>Tech Stack</h2>
+        <h4>Front End</h4>
+        <ul>
+            <li>React</li>
+            <li>Redux
+                <ul>
+                    <li>react-redux</li>
+                    <li>redux-multi</li>
+                    <li>redux-promise</li>
+                    <li>redux-thunk</li>
+                </ul>
+            </li>
+            <li>Webpack</li>
+            <li>Bootstrap</li>
+            <li>Font Awesome</li>
+        </ul>
+        <h4>Back End</h4>
+        <ul>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
+            <li>PM2</li>
+            <li>Node Restful</li>
+            <li>Body Parser</li>
+            <li>Cors</li>
+        </ul>
     </div>
 );
