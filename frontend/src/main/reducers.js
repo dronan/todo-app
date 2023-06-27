6d25ba419b04b9ee3778b3c7e1fa81d355ef5f37
Diego Ronan Kulian <dkulian@veepee.com>
2023-06-27 06:53:32 +0200

    fix reducres file

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,22 +1,25 @@
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
-    // Define a top-level state field named `todos`, handled by `todosReducer`
-    todos: () => ({
-        _id: 1,
-        description: 'Pagar fatura do cartão',
-        done: true
-    },
-    {
-        _id: 2,
-        description: 'Reunião com a equipe às 10:00',
-        done: false
-    },
-    {
-        _id: 3,
-        description: 'Consulta médica na terça depois do almoço',
-        done: false
-    }),
+    todo: () => ({
+        description: 'Ler livro',
+        list: [{
+                _id: 1,
+                description: 'Pagar fatura do cartão',
+                done: true
+            },
+            {
+                _id: 2,
+                description: 'Reunião com a equipe às 10:00',
+                done: false
+            },
+            {
+                _id: 3,
+                description: 'Consulta médica na terça depois do almoço',
+                done: false
+            }
+        ]
+    })
 });
 
 export default rootReducer;
