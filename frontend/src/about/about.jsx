cc987c91fcb9a4ba65ae4a84ca290bc8c017e359
Diego Ronan Kulian <dkulian@veepee.com>
2023-06-27 15:05:16 +0200

    add shotcuts info

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -7,6 +7,18 @@ export default props => (
         <h2>Porpouse</h2>
         <p>It's a simple task manager.</p>
         <p>It was developed with React with Redux making communications between components on a easy way.</p>
+        <h2>Shortcuts</h2>
+        <ul>
+            <li>
+                <strong>Enter</strong> - Add a new task
+            </li>
+            <li>
+                <strong>Shift + Enter</strong> - Search a task
+            </li>
+            <li>
+                <strong>Esc</strong> - Clear the search
+            </li>
+        </ul>
         <h2>Tech Stack</h2>
         <h4>Front End</h4>
         <ul>
